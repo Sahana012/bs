8d037fa3e9906aef1e75629753d7dac622a1f686
Sahana012 <74991343+Sahana012@users.noreply.github.com>
2021-03-19 15:55:17 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBDoTfuyMUQqQwiBi5zyYQYCoGSBIw-Ytk",
+    authDomain: "booksanta-47265.firebaseapp.com",
+    projectId: "booksanta-47265",
+    storageBucket: "booksanta-47265.appspot.com",
+    messagingSenderId: "513347024539",
+    appId: "1:513347024539:web:0460dac169296cfa1f783e"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
